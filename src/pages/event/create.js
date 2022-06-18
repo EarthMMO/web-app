0518269aa6a9f1704b704c58ee582ebf4493f93c
thomas-vu <thomas.vu@ucalgary.ca>
2022-06-17 20:19:49 -0600

    fixing deploy issues

--- src/pages/event/create.js
+++ src/pages/event/create.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "../../components/navbar";
+import Navbar from "../../components/Navbar";
 import CreateEventForm from "../../components/CreateEvent";
 import {
   updateProviderAndContract,
